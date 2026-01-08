7ec178c5508d118b43c92af577a5ad21d33e5792
reddishJade <dwei2615@gmail.com>
2026-01-08 21:26:42 +0800

    Add user input args support

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-from google import genai
 
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=args.user_prompt,
 )
 
 # Verify usage_metadata is not None
